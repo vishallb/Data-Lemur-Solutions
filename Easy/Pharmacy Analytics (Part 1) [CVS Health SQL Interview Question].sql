f759f9ad1b42ee6991bd097d0acddb06a9c2b77a
Vishal B <63901924+vishallb@users.noreply.github.com>
2023-08-24 18:54:06 +0530

    Create Pharmacy Analytics (Part 1) [CVS Health SQL Interview Question].sql

---
+++ Easy/Pharmacy Analytics (Part 1) [CVS Health SQL Interview Question].sql
@@ -0,0 +1,20 @@
+/* CVS Health is trying to better understand its pharmacy sales, and how well different products are selling. Each drug can only be produced by one manufacturer.
+
+Write a query to find the top 3 most profitable drugs sold, and how much profit they made. 
+Assume that there are no ties in the profits. Display the result from the highest to the lowest total profit.*/
+
+# Solution
+WITH cte as (
+  SELECT
+    drug,
+    (total_sales - cogs) as total_profit
+  FROM
+    pharmacy_sales)
+    
+SELECT
+  drug,
+  total_profit
+FROM
+  cte
+ORDER BY total_profit DESC
+LIMIT 3;
